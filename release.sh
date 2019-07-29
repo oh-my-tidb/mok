8432fca1618c87dad619acc9fa5d848603a547c3
disksing <i@disksing.com>
2019-07-29 11:11:43 +0800

    add release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir releases
+GOOS=darwin GOARCH=amd64 go build -o releases/mok-$1-darwin-amd64/mok
+GOOS=linux GOARCH=amd64 go build -o releases/mok-$1-linux-amd64/mok
+GOOS=windows GOARCH=amd64 go build -o releases/mok-$1-windows-amd64/mok.exe
+
+cd releases
+zip -r mok-$1-darwin-amd64.zip mok-$1-darwin-amd64
+zip -r mok-$1-linux-amd64.zip mok-$1-linux-amd64
+zip -r mok-$1-windows-amd64.zip mok-$1-windows-amd64
+
+rm -r mok-$1-darwin-amd64
+rm -r mok-$1-linux-amd64
+rm -r mok-$1-windows-amd64
